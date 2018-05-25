aa2ae433942a510d765cb9c11ed396ee98c347d1
Andrei Gavrilov <agavrilov@atb.com>
2018-05-25 11:11:19 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+          <h2>
+              Hello World by AG! Built by CJE Jenkins))
+          </h2>
+          <h3>wow that's simple!</h3>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
